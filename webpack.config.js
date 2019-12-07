34c7b77d1d649e42d165b87811306e172dba452a
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-12-07 13:36:47 +0000

    installed and cofigured webpack for further app development

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const postCSSPlugins = [
+  require("postcss-simple-vars"),
+  require("postcss-nested"),
+  require("autoprefixer")
+];
+
+module.exports = {
+  //configuration object
+  entry: "./app/assets/scripts/App.js",
+  output: {
+    // creating output to designated path with chosen name for the file
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app")
+  },
+  mode: "development",
+  watch: true, //auto-generate 'pm run dev' (in package.json:   scripts: {"dev": "webpack"}) to stop ctrl + c in terminal
+  module: {
+    rules: [
+      {
+        test: /\.css$/i, //regular expression - for everything that endds with .css use css-loader
+        use: [
+          "style-loader",
+          "css-loader",
+          { loader: "postcss-loader", options: { plugins: postCSSPlugins } }
+        ] //index 2 in this array is object setting up postcss-loader
+      }
+    ]
+  }
+};
