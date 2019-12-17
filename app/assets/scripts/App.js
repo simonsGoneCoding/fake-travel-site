ed0cefc63c4698e6007fb28169d584894ced58e7
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-12-17 22:46:30 +0000

    added js functionality to header menu for small screens

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,9 @@
 import "../styles/styles.css";
+import MobileMenu from "./modules/MobileMenu";
+
+const mobileMenu = new MobileMenu();
 
 if (module.hot) {
+  // enables hot modual replacement
   module.hot.accept();
 }
