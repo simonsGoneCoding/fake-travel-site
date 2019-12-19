85882941061da2471d2511db39f19f957dc621cc
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-12-19 22:25:32 +0000

    added styling to Header Nav Menu for small screens

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -2,13 +2,16 @@ class MobileMenu {
   constructor() {
     this.menuIcon = document.querySelector(".site-header__menu-icon");
     this.menuContent = document.querySelector(".site-header__menu-content");
+    this.siteHeader = document.querySelector(".site-header");
     this.events();
   }
   events() {
     this.menuIcon.addEventListener("click", () => this.toggleTheMenu());
   }
+
   toggleTheMenu() {
     this.menuContent.classList.toggle("site-header__menu-content--active");
+    this.siteHeader.classList.toggle("site-header--active");
   }
 }
 
