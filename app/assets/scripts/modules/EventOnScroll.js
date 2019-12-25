7aa2be4fe85fd102205bf623d72e8016b9fbc910
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-12-25 18:40:16 +0000

    updated app with npm lodash package and added optimized performance-based scroll functionality

--- app/assets/scripts/modules/EventOnScroll.js
+++ app/assets/scripts/modules/EventOnScroll.js
@@ -1,20 +1,25 @@
+import throttle from "lodash/throttle";
+
 class EventOnScroll {
   constructor() {
     this.itemsToReveal = document.querySelectorAll(".feature-item");
     this.hideInitially();
-    this.evenst();
+    this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
+    this.events();
   }
 
-  evenst() {
-    window.addEventListener("scroll", () =>
-      this.itemsToReveal.forEach(item => this.calculateIfScrolledTo(item))
-    );
+  events() {
+    window.addEventListener("scroll", this.scrollThrottle);
+  }
+
+  calcCaller() {
+    this.itemsToReveal.forEach(item => this.calculateIfScrolledTo(item));
   }
 
   calculateIfScrolledTo(item) {
     // console.log(item.getBoundingClientRect().y);
     const scrollPercent =
-      (item.getBoundingClientRect().y / window.innerHeight) * 100;
+      (item.getBoundingClientRect().top / window.innerHeight) * 100;
     // console.log(scrollPercent + " %");
     if (scrollPercent < 99) {
       item.classList.add("reveal-item--is-visible");
