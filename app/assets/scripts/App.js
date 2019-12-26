c71effa691c3ee77ae5eca5fa199bb655cf23aa1
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-12-26 10:38:45 +0000

    Added on scroll funcionality to header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,11 @@
 import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
 import EventOnScroll from "./modules/EventOnScroll";
+import StickyHeader from "./modules/StickyHeader";
 
 const mobileMenu = new MobileMenu();
+
+new StickyHeader();
 
 new EventOnScroll(document.querySelectorAll(".feature-item"), 70); // reusable class
 new EventOnScroll(document.querySelectorAll(".testimonial"), 65);
