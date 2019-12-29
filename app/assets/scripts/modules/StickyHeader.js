05b515b8bad636f86fcce5fad47c430c6bcfc759
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-12-29 18:12:33 +0000

    Fully completed sticky header behaviour.

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,8 +1,12 @@
 import throttle from "lodash/throttle";
+import debounce from "lodash/debounce";
 
 class StickyHeader {
   constructor() {
+    this.browserHeight = window.innerHeight;
     this.siteHeader = document.querySelector(".site-header");
+    this.pageSections = document.querySelectorAll(".page-section");
+    this.previousScrollY = window.scrollY;
     this.events();
   }
 
@@ -11,13 +15,53 @@ class StickyHeader {
       "scroll",
       throttle(() => this.runOnScroll(), 200)
     );
+    window.addEventListener(
+      "resize", // re-calculating window height if window size changed
+      debounce(() => {
+        this.browserHeight = window.innerHeight;
+      }, 250)
+    );
   }
 
   runOnScroll() {
+    this.determineScrollDirection();
+
     if (window.scrollY > 120) {
       this.siteHeader.classList.add("site-header--dark");
     } else {
       this.siteHeader.classList.remove("site-header--dark");
+    }
+    this.pageSections.forEach(el => this.calcSection(el));
+  }
+
+  determineScrollDirection() {
+    if (window.scrollY > this.previousScrollY) {
+      this.scrollDirection = "down";
+    } else {
+      this.scrollDirection = "up";
+    }
+    this.previousScrollY = window.scrollY;
+  }
+
+  calcSection(el) {
+    if (
+      window.scrollY + this.browserHeight > el.offsetTop &&
+      window.scrollY < el.offsetTop + el.offsetHeight
+    ) {
+      let scrollPercent =
+        (el.getBoundingClientRect().top / this.browserHeight) * 100;
+      if (
+        (scrollPercent < 18 &&
+          scrollPercent > -0.1 &&
+          this.scrollDirection === "down") ||
+        (scrollPercent < 33 && this.scrollDirection === "up")
+      ) {
+        let matchingLink = el.getAttribute("data-matching-link");
+        document
+          .querySelectorAll(`.primary-nav a:not(${matchingLink})`)
+          .forEach(el => el.classList.remove("is-current-link"));
+        document.querySelector(matchingLink).classList.add("is-current-link");
+      }
     }
   }
 }
