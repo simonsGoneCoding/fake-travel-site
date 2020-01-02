62009c32826720eff2913e1b2e2580d71f620852
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-01-02 19:14:42 +0000

    Completed modal js behaviour

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,9 +2,11 @@ import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
 import EventOnScroll from "./modules/EventOnScroll";
 import StickyHeader from "./modules/StickyHeader";
+import Modal from "./modules/Modal";
 
 const mobileMenu = new MobileMenu();
 
+new Modal();
 new StickyHeader();
 
 new EventOnScroll(document.querySelectorAll(".feature-item"), 70); // reusable class
